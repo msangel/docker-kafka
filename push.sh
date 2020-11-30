9f2778c32d84ac019ceed857b5bd14684c94371d
Vasyl Khrystiuk <h6.msangel@gmail.com>
2020-11-30 18:18:33 +0200

    update script

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 
 # https://ropenscilabs.github.io/r-docker-tutorial/04-Dockerhub.html
 
-docker tag docker-kafka-interactive msangel/docker-kafka-interactive:initial
-docker push msangel/docker-kafka-interactive:initial
+docker tag docker-kafka-interactive msangel/docker-kafka-interactive:latest
+docker push msangel/docker-kafka-interactive:latest
